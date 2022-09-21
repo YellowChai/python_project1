16e26bd432faeb8b922c20cf3bcea78ba4239cc2
alice <aliceahuh213@gmail.com>
2022-09-21 18:11:49 -0400

    add pseudo code

---
+++ magic.py
@@ -0,0 +1,29 @@
+import random
+
+# variable: user_input(string), user_answer(list), answer_dict(dictionary number(1-15):answer_list)   
+# disply list of numbers A, and ask if the number is in this list
+# save the answer in the list (yes=1 no=0)
+# display list of numbers B, and ask if the number is in this list
+# append the answer to the answer list (yes=1 no=0)
+# display list of number C, and ask if the number is in this list
+# append the answer to the answer list (yes=1 no=0)
+# display list of number D, and ask if the number is in this list 
+# append the answer to the answer list (yes=1 no=0)
+# check the dictionary for the guessing number (key= user_number value= answer_list) 
+# display the answer and ask the user if the answer is correct 
+# - if yes: exit the while loop 
+# - no: let me try it again (yes or no )
+#       -if yes:
+#           start from the beginning
+#       -if no:
+#           exit the while loop
+
+
+# possible errors
+# invalid input 
+# A. number input when needed yes or no - can be solved by 1. provide both options or 2. error message to choose the word yes or no 
+# B. Capital letter input - make all lowercase 
+# C. Y or YES - allow both input
+# D. invalid letters - error message(A can be solved with this)
+
+#
